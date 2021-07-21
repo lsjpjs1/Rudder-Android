4487eb8ffc0da2dae683209dff31dd9bf8e5d8b9
lsjpjs1 <lsjpjs1@naver.com>
2021-07-21 19:39:04 +0900

    fix : 게시글 리사이클러뷰 칸 띄워지게끔 수정

--- app/src/main/java/com/rudder/ui/fragment/CommunityHeaderFragment.kt
+++ app/src/main/java/com/rudder/ui/fragment/CommunityHeaderFragment.kt
@@ -1,20 +1,29 @@
 package com.rudder.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.rudder.R
+import com.rudder.databinding.FragmentCommunityHeaderBinding
+import com.rudder.viewModel.MainViewModel
 
 class CommunityHeaderFragment : Fragment() {
+    private val viewModel = MainViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-
-        return inflater.inflate(R.layout.fragment_community_header, container, false)
+        val header = DataBindingUtil.inflate<FragmentCommunityHeaderBinding>(inflater,R.layout.fragment_community_header,container,false)
+        header.imageView.setOnClickListener{
+            Log.d("click","click")
+        }
+        header.mainVM = viewModel
+        return header.root
     }
 }
