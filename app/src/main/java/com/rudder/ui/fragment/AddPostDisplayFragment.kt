70f0df65922de2b8bbe78cb605f1e757719108f0
lsjpjs1 <lsjpjs1@naver.com>
2021-07-28 20:37:33 +0900

    fix : add post 레이아웃 수정

--- app/src/main/java/com/rudder/ui/fragment/AddPostDisplayFragment.kt
+++ app/src/main/java/com/rudder/ui/fragment/AddPostDisplayFragment.kt
@@ -1,15 +1,18 @@
 package com.rudder.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.rudder.R
 import com.rudder.databinding.FragmentAddPostDisplayBinding
 import com.rudder.databinding.FragmentAddPostHeaderBinding
 import com.rudder.viewModel.MainViewModel
+import kotlinx.android.synthetic.main.fragment_add_post_display.*
 
 class AddPostDisplayFragment : Fragment() {
     private val viewModel = MainViewModel
@@ -18,9 +21,8 @@ class AddPostDisplayFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val header = DataBindingUtil.inflate<FragmentAddPostDisplayBinding>(inflater,
+        val display = DataBindingUtil.inflate<FragmentAddPostDisplayBinding>(inflater,
                 R.layout.fragment_add_post_display,container,false)
-        //header.mainVM = viewModel
-        return header.root
+        return display.root
     }
 }
