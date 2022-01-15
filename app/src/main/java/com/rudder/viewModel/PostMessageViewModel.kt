55a9856aa060b540405cc758166a8dad066e2b47
2taezeat <xorudfl772@naver.com>
2022-01-15 13:20:27 +0900

    test : 21 01 15 real final

--- app/src/main/java/com/rudder/viewModel/PostMessageViewModel.kt
+++ app/src/main/java/com/rudder/viewModel/PostMessageViewModel.kt
@@ -7,17 +7,19 @@ import java.sql.Timestamp
 import java.text.SimpleDateFormat
 
 class PostMessageViewModel: ViewModel() {
+    /// 21 01 15 real final
 
     fun getPostMessages() : ArrayList<PostMessage> {
         return arrayListOf<PostMessage>(
             PostMessage(
-            receiveUserInfoId = 0,
-            sendUserInfoId = 0,
-            messageBody = "",
-            isRead = false,
-            messageSendTime = Timestamp.valueOf("2021-07-13 11:11:11"),
-            postMessageId = 0,
-            sendUserNickname = "tjdgns"
+                receiveUserInfoId = 0,
+                sendUserInfoId = 0,
+                messageBody = "",
+                isRead = false,
+                messageSendTime = Timestamp.valueOf("2021-07-13 11:11:11"),
+                postMessageId = 0,
+                sendUserNickname = "tjdgns",
+                receiveUserNickname = "tmp1"
             ),
             PostMessage(
                 receiveUserInfoId = 0,
@@ -26,7 +28,8 @@ class PostMessageViewModel: ViewModel() {
                 isRead = false,
                 messageSendTime = Timestamp.valueOf("2021-07-13 11:11:11"),
                 postMessageId = 0,
-                sendUserNickname = "xorud"
+                sendUserNickname = "xorud",
+                receiveUserNickname = "tmp2"
             ),
             PostMessage(
                 receiveUserInfoId = 0,
@@ -35,7 +38,8 @@ class PostMessageViewModel: ViewModel() {
                 isRead = false,
                 messageSendTime = Timestamp.valueOf("2021-07-13 11:11:11"),
                 postMessageId = 0,
-                sendUserNickname = "alsgh"
+                sendUserNickname = "alsgh",
+                receiveUserNickname = "tmp3"
             )
         )
     }
