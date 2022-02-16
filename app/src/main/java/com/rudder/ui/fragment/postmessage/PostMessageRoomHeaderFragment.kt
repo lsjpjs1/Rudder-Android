2fff3db96b492b55f15666de257563b6e7943f34
2taezeat <xorudfl772@naver.com>
2022-02-16 14:06:19 +0900

    fix : 가버전 (22 02 16, 14:05) pull 및 notification 복원

--- app/src/main/java/com/rudder/ui/fragment/postmessage/PostMessageRoomHeaderFragment.kt
+++ app/src/main/java/com/rudder/ui/fragment/postmessage/PostMessageRoomHeaderFragment.kt
@@ -42,9 +42,9 @@ class PostMessageRoomHeaderFragment:Fragment() {
         return header.root
     }
 
-    fun goBack(){
-        activity?.onBackPressed()
-    }
+//    fun goBack(){
+//        activity?.onBackPressed()
+//    }
 
     fun showSendPostMessageDialog() {
         val receiveUserInfoId = viewModel.targetUserInfoId
