76d5b8b563414f5ab92c03957ce2d3d28cb79343
2taezeat <xorudfl772@naver.com>
2022-04-20 18:06:54 +0900

    feat : JobsDetailsFragment.kt apply button Intent URL

--- app/src/main/java/com/rudder/ui/fragment/jobs/JobsDetailsFragment.kt
+++ app/src/main/java/com/rudder/ui/fragment/jobs/JobsDetailsFragment.kt
@@ -1,5 +1,7 @@
 package com.rudder.ui.fragment.jobs
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -134,8 +136,15 @@ class JobsDetailsFragment : Fragment(), CustomOnclickListener {
                 view.jobsDetailHeart.setColorFilter(ContextCompat.getColor(lazyContext,R.color.grey))
                 view.jobsDetailHeart.tag = "border"
                 it.background = ContextCompat.getDrawable(lazyContext, R.drawable.edge_grey)
-
             }
+
+        }
+
+
+        view.jobsDetailApplyButton.setOnClickListener {
+            val url = "https://www.naver.com"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
 
         }
 
